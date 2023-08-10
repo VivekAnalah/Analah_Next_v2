ce148ecf73a68f72d945f3b8de20ba20d77efba4
unknown <vivek.belwal@analah.in>
2023-08-10 18:59:56 +0530

    started the next js version

---
+++ analah-next-v2/app/page.js
@@ -0,0 +1,57 @@
+// import React from 'react'
+// import { Header } from '../Components/Header'
+// import "../App.css";
+// import "../Styles/home.css"
+// import { HomeTopSection } from '../Components/HomeTopSection';
+// import { Choose } from '../Components/Choose';
+// import OurPartner from '../Components/OurPartner';
+// import { About } from '../Components/About';
+// import { Value } from '../Components/Value';
+// import { FoundingTeam } from '../Components/FoundingTeam';
+// import Footer from '../Components/Footer';
+
+export  default function Home() {
+
+ 
+
+  return (
+    <div className=''>
+      {/* <Header /> */}
+
+
+    
+    {/* <HomeTopSection />  */}
+     
+     
+
+     {/* <Choose />    */}
+
+     <div >
+     <div className="relative">
+          <img
+            alt="Right triangle"
+            // src={require("../Assets/Images/rightTriangle.png")}
+            className="object-contain aspect-auto h-auto absolute right-0 sm:w-[100px] w-[70px] sm:top-[-3.5%] sm:block hidden "
+          />
+         
+            {/* <OurPartner /> */}
+
+            
+        
+        </div>
+     </div>
+     {/* <About /> */}
+
+     {/* <Value /> */}
+
+     {/* <FoundingTeam /> */}
+
+     {/* <Footer /> */}
+    
+
+
+
+       
+      </div>
+  )
+}
